2e501b4a13ac7e7525b8884e609bed03f69892c2
pafnuteva_ae <pafnuteva_ae@farpost.com>
2023-12-09 09:16:22 +1000

    Initial commit

---
+++ get_participants.py
@@ -0,0 +1,31 @@
+import logging
+
+import requests
+import logging
+
+
+def download_scorecards(logger=logging.getLogger()):
+    logger.info(f'Try to download scorecards')
+    try:
+        cookies = {}
+        login_info = {
+            'username': 'arina@pafnuteva.ru',
+            'password': 'yuhv82kd',
+            'cookieYes': 0
+        }
+        try_login = requests.post('https://my.spotonlearning.eu/ehio/site/user.cfc?method=doLogin',
+                                  data=login_info)
+        cookies['JSESSIONID'] = try_login.headers.get('JSESSIONID')
+        cookies['CFID'] = try_login.headers.get('CFID')
+        cookies['CFTOKEN'] = try_login.headers.get('CFTOKEN')
+
+        try_request_participants = requests.get('https://my.spotonlearning.eu/manage/excel/output/participants.xls',
+                                                cookies=cookies)
+        with open('participants.xls', 'wb') as f:
+            f.write(try_request_participants.content)
+        logger.info(f'Downloading and saving finished successful')
+        return True
+    except Exception as e:
+        logger.info(f'Downloading and saving was failed')
+        logger.error(str(e))
+        return False
